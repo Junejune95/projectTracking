fd5a6afd78fc05e1ce1e75b22905c1f770df926f
zune <seinmama95@gmail.com>
2022-02-03 07:14:09 -0800

    add basic project route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('../app/module/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'project',
+    loadChildren: () => import('../app/module/project/project.module').then((m) => m.ProjectModule)
+  },
+  {
+    path: 'task',
+    loadChildren: () => import('../app/module/task/task.module').then((m) => m.TaskModule)
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/module/project/project.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectComponent } from './project/project.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectComponent,
+  }
+];
+
+
+
+@NgModule({
+  declarations: [
+    ProjectComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ProjectModule { }
