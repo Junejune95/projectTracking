5201abcd8e0662e44c7bf1c1be011c248cd8023d
zune <seinmama95@gmail.com>
2022-02-05 19:46:13 -0800

    add toolbar search and create button

--- src/app/module/project/project.module.ts
+++ src/app/module/project/project.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectComponent } from './project/project.component';
 import { RouterModule, Routes } from '@angular/router';
+
+// material module
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { BasicComponentsModule } from 'app/shared/basic-components/basic-components.module';
 
 const routes: Routes = [
@@ -18,8 +27,13 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    BasicComponentsModule
-
+    BasicComponentsModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
   ]
 })
 export class ProjectModule { }
